2789b1b055a10b0b60317a8a3782e132ecae182d
h-i0610 <youjingneng@gmail.com>
2024-11-07 18:12:05 +0900

    base.html作成

--- fitnessapp/urls.py
+++ fitnessapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/',views.LoginView.as_view(),name='login'),
     path('signup/',views.SignupView.as_view(),name='signup'),
     path('signup_complete/',views.SignupCompleteView.as_view(),name='signup_complete'),
+    path('home/',views.HomeView.as_view(),name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,23 +4,26 @@ from django.shortcuts import redirect
 from django.views.generic import CreateView,TemplateView
 from .forms import LoginForm,SignupForm
 from django.contrib.auth.views import LoginView,LogoutView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy,reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 class LoginView(LoginView):
     template_name = "login.html"
     form_class = LoginForm
+    
+    def get_success_url(self):
+        return reverse('home')
 
     def form_valid(self,form):
-        response = super().form_valid(form)
 
         if not form.cleaned_data.get('remember_me'):
             self.request.session.set_expiry(0)
         else:
             self.request.session.set_expiry(1209600)
 
-        return response
+        return super().form_valid(form)
     
 class SignupView(CreateView):
     template_name = 'signup.html'
@@ -37,4 +40,7 @@ class SignupView(CreateView):
     
 class SignupCompleteView(TemplateView):
     template_name = 'signup_complete.html'
+
+class HomeView(LoginRequiredMixin,TemplateView):
+    template_name = 'home.html'
         
